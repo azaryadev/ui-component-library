9fbfea89e158d44fc9a895c2c7f01ca51b9c9cf1
Aldy Azarya <aldy@autentika.id>
2024-11-18 10:36:51 +0700

    feat: add typescript and rollup config

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+// ... existing code ...
+module.exports = {
+  plugins: {
+    "tailwindcss/nesting": {},
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+// ... existing code ...
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import "tailwindcss/tailwind.css";
+import "@/assets/styles/tailwind/index.css";
+
+import "@/assets/styles/components/index.css";
+
+export * from "./components";
